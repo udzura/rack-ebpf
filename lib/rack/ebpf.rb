7014438560750fc371296aeb55cc3f54cc33d9fb
Uchio Kondo <udzura@udzura.jp>
2020-03-02 16:25:26 +0900

    Initial

---
+++ lib/rack/ebpf.rb
@@ -0,0 +1,8 @@
+require "rack/ebpf/version"
+
+module Rack
+  module Ebpf
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/ebpf/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Ebpf
+    VERSION = "0.1.0"
+  end
+end
